cf48bbe8fa7e0b550a3c171bae15726bb3a17e33
Mihai <capy.michael@gmail.com>
2024-10-03 09:21:54 +0000

    fizzbuzz

--- 01_03b/1-3-fizzbuzz.php
+++ 01_03b/1-3-fizzbuzz.php
@@ -1,1 +1,27 @@
+<style>
+  .num {
+    background-color: white;
+    color: black;
+  }
+</style>
 <?php
+function checkNumber($number)
+{
+  if ($number % 3 == 0 && $number % 5 != 0) {
+    return "Fizz";
+  } elseif ($number % 5 == 0 && $number % 3 != 0) {
+    return "Buzz";
+  } elseif ($number % 5 == 0 && $number % 3 == 0) {
+    return "FizzBuzz";
+  }
+  return $number;
+}
+?>
+<div class="num">
+  <?php
+  $maxNumbers = 100;
+  for ($i = 1; $i <= $maxNumbers; $i++) {
+    echo checknumber($i) . "<br>";
+  }
+  ?>
+</div>
